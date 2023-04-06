b5aa9f73f26886cb98bbe240d5f7944594334b85
jhalak1509 <surve.j@northeastern.edu>
2023-04-06 15:03:19 -0400

    Added Database Credentials

---
+++ src/dbConstants/DatabaseConstants.java
@@ -0,0 +1,8 @@
+package dbConstants;
+
+public class DatabaseConstants {
+
+	public static String username = "root";
+	public static String url = "jdbc:mysql://localhost:3306/SmartCart";
+	public static String password = "root";
+}
