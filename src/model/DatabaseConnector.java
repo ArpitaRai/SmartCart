067b7e4895094d90bfba4a0641a28d85470a20be
jhalak1509 <surve.j@northeastern.edu>
2023-04-06 17:57:16 -0400

    Database Connection

--- src/model/DatabaseConnector.java
+++ src/model/DatabaseConnector.java
@@ -1,5 +1,30 @@
 package model;
 
+import java.sql.*;
+
+import dbConstants.DatabaseConstants;
+
 public class DatabaseConnector {
 
+private static Connection databaseConnection = null;
+	
+	private static Statement st; 
+
+	private DatabaseConnector() {
+	}
+	
+	// Singleton class instance to avoid multiple Database connections
+	// Returns Database connection instance
+	public static Connection getInstance() throws Exception {
+		if (databaseConnection == null) {
+			synchronized (DatabaseConnector.class) {
+				if (databaseConnection == null) {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					databaseConnection = DriverManager.getConnection(DatabaseConstants.url,
+							DatabaseConstants.username, DatabaseConstants.password);
+				}
+			}
+		}
+		return databaseConnection;
+	}
 }
