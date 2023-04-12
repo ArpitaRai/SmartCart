390db9d5cd3d3a935025c433310b872fc4d5a50d
jhalak1509 <surve.j@northeastern.edu>
2023-04-12 16:58:07 -0400

    Changes to Dairy Controller

--- src/controller/DairyController.java
+++ src/controller/DairyController.java
@@ -15,35 +15,35 @@ import model.Product;
 
 public class DairyController extends ProductBaseController {
 
-	@FXML Label milkPrice;
-	@FXML Label butterPrice;
-	@FXML Label heavyCreamPrice;
-	@FXML Label iceCreamPrice;
-	@FXML Label chocolateMilkPrice;
-	@FXML Label sourCreamPrice;
-	@FXML Label stringCheesePrice;
-	@FXML Label mozzarellaCheesePrice;
-	@FXML Label yogurtPrice;
+	@FXML Label lblMilkPrice;
+	@FXML Label lblButterPrice;
+	@FXML Label lblHeavyCreamPrice;
+	@FXML Label lblIceCreamPrice;
+	@FXML Label lblChocolateMilkPrice;
+	@FXML Label lblSourCreamPrice;
+	@FXML Label lblStringCheesePrice;
+	@FXML Label lblMozzarellaCheesePrice;
+	@FXML Label lblYogurtPrice;
 
-	@FXML Spinner<Integer> milkSpinner;
-	@FXML Spinner<Integer> butterSpinner;
-	@FXML Spinner<Integer> heavyCreamSpinner;
-	@FXML Spinner<Integer> iceCreamSpinner;
-	@FXML Spinner<Integer>chocolateMilkSpinner;
-	@FXML Spinner<Integer> sourCreamSpinner;
-	@FXML Spinner<Integer> stringCheeseSpinner;
-	@FXML Spinner<Integer> mozzarellaCheeseSpinner;
-	@FXML Spinner<Integer> yogurtSpinner;
+	@FXML Spinner<Integer> spinMilk;
+	@FXML Spinner<Integer> spinButter;
+	@FXML Spinner<Integer> spinHeavyCream;
+	@FXML Spinner<Integer> spinIceCream;
+	@FXML Spinner<Integer>spinChocolateMilk;
+	@FXML Spinner<Integer> spinSourCream;
+	@FXML Spinner<Integer> spinStringCheese;
+	@FXML Spinner<Integer> spinMozzarellaCheese;
+	@FXML Spinner<Integer> spinYogurt;
 
-	@FXML Button milkButton;
-	@FXML Button butterButton;
-	@FXML Button heavyCreamButton;
-	@FXML Button iceCreamButton;
-	@FXML Button chocolateMilkButton;
-	@FXML Button sourCreamButton;
-	@FXML Button stringCheeseButton;
-	@FXML Button mozzarellaCheeseButton;
-	@FXML Button yogurtButton;
+	@FXML Button btnMilk;
+	@FXML Button btnButter;
+	@FXML Button btnHeavyCream;
+	@FXML Button btnIceCream;
+	@FXML Button btnChocolateMilk;
+	@FXML Button btnSourCream;
+	@FXML Button btnStringCheese;
+	@FXML Button btnMozzarellaCheese;
+	@FXML Button btnYogurt;
 
 	@Override
 	public void initialize() {
@@ -59,69 +59,69 @@ public class DairyController extends ProductBaseController {
 			e.printStackTrace();
 		}
 
-		milkPrice.setText("$" + inventoryItems.get("DAI001").getPrice());
-		butterPrice.setText("$" + inventoryItems.get("DAI002").getPrice());
-		heavyCreamPrice.setText("$" + inventoryItems.get("DAI003").getPrice());
-		iceCreamPrice.setText("$" + inventoryItems.get("DAI004").getPrice());
-		chocolateMilkPrice.setText("$" + inventoryItems.get("DAI005").getPrice());
-		sourCreamPrice.setText("$" + inventoryItems.get("DAI006").getPrice());
-		stringCheesePrice.setText("$" + inventoryItems.get("DAI007").getPrice());
-		mozzarellaCheesePrice.setText("$" + inventoryItems.get("DAI008").getPrice());
-		yogurtPrice.setText("$" + inventoryItems.get("DAI009").getPrice());
+		lblMilkPrice.setText("$" + inventoryItems.get("DAI001").getPrice());
+		lblButterPrice.setText("$" + inventoryItems.get("DAI002").getPrice());
+		lblHeavyCreamPrice.setText("$" + inventoryItems.get("DAI003").getPrice());
+		lblIceCreamPrice.setText("$" + inventoryItems.get("DAI004").getPrice());
+		lblChocolateMilkPrice.setText("$" + inventoryItems.get("DAI005").getPrice());
+		lblSourCreamPrice.setText("$" + inventoryItems.get("DAI006").getPrice());
+		lblStringCheesePrice.setText("$" + inventoryItems.get("DAI007").getPrice());
+		lblMozzarellaCheesePrice.setText("$" + inventoryItems.get("DAI008").getPrice());
+		lblYogurtPrice.setText("$" + inventoryItems.get("DAI009").getPrice());
 
-		milkSpinner.setValueFactory(
+		spinMilk.setValueFactory(
 				new SpinnerValueFactory.IntegerSpinnerValueFactory(0, inventoryItems.get("DAI001").getQuantity(), 0));
 
-		butterSpinner.setValueFactory(
+		spinButter.setValueFactory(
 				new SpinnerValueFactory.IntegerSpinnerValueFactory(0, inventoryItems.get("DAI002").getQuantity(), 0));
 
-		heavyCreamSpinner.setValueFactory(
+		spinHeavyCream.setValueFactory(
 				new SpinnerValueFactory.IntegerSpinnerValueFactory(0, inventoryItems.get("DAI003").getQuantity(), 0));
 
-		iceCreamSpinner.setValueFactory(
+		spinIceCream.setValueFactory(
 				new SpinnerValueFactory.IntegerSpinnerValueFactory(0, inventoryItems.get("DAI004").getQuantity(), 0));
 
-		chocolateMilkSpinner.setValueFactory(
+		spinChocolateMilk.setValueFactory(
 				new SpinnerValueFactory.IntegerSpinnerValueFactory(0, inventoryItems.get("DAI005").getQuantity(), 0));
 
-		sourCreamSpinner.setValueFactory(
+		spinSourCream.setValueFactory(
 				new SpinnerValueFactory.IntegerSpinnerValueFactory(0, inventoryItems.get("DAI006").getQuantity(), 0));
 
-		stringCheeseSpinner.setValueFactory(
+		spinStringCheese.setValueFactory(
 				new SpinnerValueFactory.IntegerSpinnerValueFactory(0, inventoryItems.get("DAI007").getQuantity(), 0));
 
-		mozzarellaCheeseSpinner.setValueFactory(
+		spinMozzarellaCheese.setValueFactory(
 				new SpinnerValueFactory.IntegerSpinnerValueFactory(0, inventoryItems.get("DAI008").getQuantity(), 0));
 
-		yogurtSpinner.setValueFactory(
+		spinYogurt.setValueFactory(
 				new SpinnerValueFactory.IntegerSpinnerValueFactory(0, inventoryItems.get("DAI009").getQuantity(), 0));
 		
 		if(inventoryItems.get("DAI001").getQuantity() == 0) {
-			setOutOfStockField(milkPrice, milkSpinner, milkButton);
+			setOutOfStockField(lblMilkPrice, spinMilk, btnMilk);
 		}
 		if(inventoryItems.get("DAI002").getQuantity() == 0) {
-			setOutOfStockField(butterPrice, butterSpinner, butterButton);
+			setOutOfStockField(lblButterPrice, spinButter, btnButter);
 		}
 		if(inventoryItems.get("DAI003").getQuantity() == 0) {
-			setOutOfStockField(heavyCreamPrice, heavyCreamSpinner, heavyCreamButton);
+			setOutOfStockField(lblHeavyCreamPrice, spinHeavyCream, btnHeavyCream);
 		}
 		if(inventoryItems.get("DAI004").getQuantity() == 0) {
-			setOutOfStockField(iceCreamPrice, iceCreamSpinner, iceCreamButton);
+			setOutOfStockField(lblIceCreamPrice, spinIceCream, btnIceCream);
 		}
 		if(inventoryItems.get("DAI005").getQuantity() == 0) {
-			setOutOfStockField(chocolateMilkPrice, chocolateMilkSpinner, chocolateMilkButton);
+			setOutOfStockField(lblChocolateMilkPrice, spinChocolateMilk, btnChocolateMilk);
 		}
 		if(inventoryItems.get("DAI006").getQuantity() == 0) {
-			setOutOfStockField(sourCreamPrice, sourCreamSpinner, sourCreamButton);
+			setOutOfStockField(lblSourCreamPrice, spinSourCream, btnSourCream);
 		}
 		if(inventoryItems.get("DAI007").getQuantity() == 0) {
-			setOutOfStockField(stringCheesePrice, stringCheeseSpinner, stringCheeseButton);
+			setOutOfStockField(lblStringCheesePrice, spinStringCheese, btnStringCheese);
 		}
 		if(inventoryItems.get("DAI008").getQuantity() == 0) {
-			setOutOfStockField(mozzarellaCheesePrice, mozzarellaCheeseSpinner, mozzarellaCheeseButton);
+			setOutOfStockField(lblMozzarellaCheesePrice, spinMozzarellaCheese, btnMozzarellaCheese);
 		}
 		if(inventoryItems.get("DAI009").getQuantity() == 0) {
-			setOutOfStockField(yogurtPrice, yogurtSpinner, yogurtButton);
+			setOutOfStockField(lblYogurtPrice, spinYogurt, btnYogurt);
 		}
 
 	}
@@ -130,71 +130,71 @@ public class DairyController extends ProductBaseController {
 	@FXML
 	private void addToCart(javafx.event.ActionEvent event) {
 
-		if (((Button) event.getTarget()).getId().toString().equals("milkButton")) {
-			CartItem ci = new CartItem("DAI001", "Milk", (Integer) milkSpinner.getValue(),
+		if (((Button) event.getTarget()).getId().toString().equals("btnMilk")) {
+			CartItem ci = new CartItem("DAI001", "Milk", (Integer) spinMilk.getValue(),
 					inventoryItems.get("DAI001").getPrice());
 
 			cart.addProduct(ci);
 
 		}
 
-		if (((Button) event.getTarget()).getId().toString().equals("butterButton")) {
-			CartItem ci = new CartItem("DAI002", "Butter", (Integer) butterSpinner.getValue(),
+		if (((Button) event.getTarget()).getId().toString().equals("btnButter")) {
+			CartItem ci = new CartItem("DAI002", "Butter", (Integer) spinButter.getValue(),
 					inventoryItems.get("DAI002").getPrice());
 
 			cart.addProduct(ci);
 
 		}
 
-		if (((Button) event.getTarget()).getId().toString().equals("heavyCreamButton")) {
-			CartItem ci = new CartItem("DAI003", "Heavy Cream", (Integer) heavyCreamSpinner.getValue(),
+		if (((Button) event.getTarget()).getId().toString().equals("btnHeavyCream")) {
+			CartItem ci = new CartItem("DAI003", "Heavy Cream", (Integer) spinHeavyCream.getValue(),
 					inventoryItems.get("DAI003").getPrice());
 
 			cart.addProduct(ci);
 
 		}
 
-		if (((Button) event.getTarget()).getId().toString().equals("iceCreamButton")) {
-			CartItem ci = new CartItem("DAI004", "Ice Cream", (Integer) iceCreamSpinner.getValue(),
+		if (((Button) event.getTarget()).getId().toString().equals("btnIceCream")) {
+			CartItem ci = new CartItem("DAI004", "Ice Cream", (Integer) spinIceCream.getValue(),
 					inventoryItems.get("DAI004").getPrice());
 
 			cart.addProduct(ci);
 
 		}
 
-		if (((Button) event.getTarget()).getId().toString().equals("chocolateMilkButton")) {
-			CartItem ci = new CartItem("DAI005", "Chocolate Milk", (Integer)chocolateMilkSpinner.getValue(),
+		if (((Button) event.getTarget()).getId().toString().equals("btnChocolateMilk")) {
+			CartItem ci = new CartItem("DAI005", "Chocolate Milk", (Integer)spinChocolateMilk.getValue(),
 					inventoryItems.get("DAI005").getPrice());
 
 			cart.addProduct(ci);
 		}
 
-		if (((Button) event.getTarget()).getId().toString().equals("sourCreamButton")) {
-			CartItem ci = new CartItem("DAI006", "Sour Cream", (Integer) sourCreamSpinner.getValue(),
+		if (((Button) event.getTarget()).getId().toString().equals("btnSourCream")) {
+			CartItem ci = new CartItem("DAI006", "Sour Cream", (Integer) spinSourCream.getValue(),
 					inventoryItems.get("DAI006").getPrice());
 
 			cart.addProduct(ci);
 
 		}
 
-		if (((Button) event.getTarget()).getId().toString().equals("stringCheeseButton")) {
-			CartItem ci = new CartItem("DAI007", "String Cheese", (Integer) stringCheeseSpinner.getValue(),
+		if (((Button) event.getTarget()).getId().toString().equals("btnStringCheese")) {
+			CartItem ci = new CartItem("DAI007", "String Cheese", (Integer) spinStringCheese.getValue(),
 					inventoryItems.get("DAI007").getPrice());
 
 			cart.addProduct(ci);
 
 		}
 
-		if (((Button) event.getTarget()).getId().toString().equals("mozzarellaCheeseButton")) {
-			CartItem ci = new CartItem("DAI008", "Mozzarella Cheese", (Integer) mozzarellaCheeseSpinner.getValue(),
+		if (((Button) event.getTarget()).getId().toString().equals("btnMozzarellaCheese")) {
+			CartItem ci = new CartItem("DAI008", "Mozzarella Cheese", (Integer) spinMozzarellaCheese.getValue(),
 					inventoryItems.get("DAI008").getPrice());
 
 			cart.addProduct(ci);
 
 		}
 
-		if (((Button) event.getTarget()).getId().toString().equals("yogurtButton")) {
-			CartItem ci = new CartItem("DAI009", "Yogurt", (Integer) yogurtSpinner.getValue(),
+		if (((Button) event.getTarget()).getId().toString().equals("btnYogurt")) {
+			CartItem ci = new CartItem("DAI009", "Yogurt", (Integer) spinYogurt.getValue(),
 					inventoryItems.get("DAI009").getPrice());
 
 			cart.addProduct(ci);
